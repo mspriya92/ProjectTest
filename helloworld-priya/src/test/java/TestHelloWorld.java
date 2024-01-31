c3f892436fc9d366ff982a7d8c09b736c8958a28
mspriya92 <91115616+mspriya@users.noreply.github.com>
2024-01-31 12:42:34 +0530

    Commit Test project from eclipse

---
+++ helloworld-priya/src/test/java/TestHelloWorld.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Heelo Priya..............!");
+	}
+	
+	@Test
+	public void testHelloWorld() {
+		System.out.println("...Hello world Testing...!");
+	}
+
+}
